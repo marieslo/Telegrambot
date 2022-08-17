606d0f28626482a0b708f3fdec0931ef3e97f160
marieslo <110108878+marieslo@users.noreply.github.com>
2022-08-17 16:10:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 TOKEN = "5475486408:AAE9J909Fz54b7788IWMzCpbaZeFsZdNI34"
 
 keys = {
-    'USD': 'USD',
-    'EUR': 'EUR',
-    'RUB': 'RUB',
-    'ILS': 'ILS'
+    'рубль': 'RUB',
+    'тенге': 'KZT',
+    'доллар': 'USD',
+    'евро': 'EUR'
+
 }
