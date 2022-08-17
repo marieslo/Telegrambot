b6f320ac8ce6ede1c324675cd2f0bd09a8bb9057
marieslo <110108878+marieslo@users.noreply.github.com>
2022-08-17 12:10:39 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,35 +1,37 @@
-import json
 import requests
-from config import exchanges
+import json
+from config import keys
 
 class APIException(Exception):
     pass
 
 
-class Convertor:
+class Converter:
     @staticmethod
-    def get_price(base, sym, amount):
+    def get_price(base_key, sym_key, amount):
         try:
-            base_key = exchanges[base.lower()]
+            base_key = keys[base_key.lower()]
         except KeyError:
-            raise APIException(f"Валюта {base} не найдена!")
+            raise APIException(f"Валюта {base_key} не найдена!")
 
         try:
-            sym_key = exchanges[sym.lower()]
+            sym_key = keys[sym_key.lower()]
         except KeyError:
-            raise APIException(f"Валюта {sym} не найдена!")
+            raise APIException(f"Валюта {sym_key} не найдена!")
 
         if base_key == sym_key:
-            raise APIException(f'Невозможно перевести одинаковые валюты {base}!')
+            raise APIException(f'Невозможно перевести одинаковые валюты {base_key}!')
 
         try:
             amount = float(amount)
         except ValueError:
             raise APIException(f'Не удалось обработать количество {amount}!')
 
-        r = requests.get(f"https://api.exchangeratesapi.io/latest?base={base_key}&symbols={sym_key}")
+        r = requests.get(
+            f"https://currate.ru/api/?get=rates&pairs={base_key}{sym_key}&key=bbe2a1ac0b909431aa237bffff3c4fca")
+        print(r.content)
         resp = json.loads(r.content)
-        new_price = resp['rates'][sym_key] * amount
-        new_price = round(new_price, 3)
-        message =  f"Цена {amount} {base} в {sym} : {new_price}"
+        new_price = float(resp['data'][base_key+sym_key]) * amount
+        new_price = round(new_price, 2)
+        message =  f"Цена {amount} {base_key} в {sym_key} : {new_price}"
         return message
